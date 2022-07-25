e81a7965b34f1c4bcee0848c8ff24b580936b4b5
ahmtburak <matrixkrista1@gmail.com>
2022-07-25 20:42:12 +0300

    25 Temmuz Çalışma

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor class newlendiği an çalışan blok
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items; // diziyi veri kaybetmemek için geçici referans tutturduk.
+            items= new T[items.Length+1]; // dizinin genişliğini artırıp alan oluşturduk.
+            for (int i = 0; i < tempArray.Length; i++) // tempArray'deki verilerimizi tekrardan items'a aktardık.
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length-1] = item; // Eklenecek olan item'ı items dizisine ekledik.
+        }
+    }
+}
